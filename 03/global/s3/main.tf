7efbd66bfeffe16478c7573bc8e7434339cf48a8
pangkin <git@pangkin.com>
2025-01-15 22:14:51 +0900

    Use terraform backend with s3

---
+++ 03/global/s3/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+# S3 버킷 설정
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "bucket-ajj-09999"
+  force_destroy = true
+
+  tags = {
+    Name = "My bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+# Dynamodb 설정
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
